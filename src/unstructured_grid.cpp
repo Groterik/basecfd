d6058d849912b53517c970505faf0ec599df9b3e
cleem <cleemmi@gmail.com>
2015-02-02 22:37:18 +0300

    Useless cells and points elimination *In progress

--- src/unstructured_grid.cpp
+++ src/unstructured_grid.cpp
@@ -1,6 +1,7 @@
 #include <basecfd/unstructured_grid.hpp>
 #include <basecfd/adjacency_list.hpp>
 #include <basecfd/adjoiner.hpp>
+#include <basecfd/grid.hpp>
 
 using namespace basecfd;
 
@@ -135,7 +136,122 @@ void UnstructuredGrid::replenishBoundary()
 
 void UnstructuredGrid::supressUseless()
 {
-    // TODO
+
+    dim_t realDimension = 2; // TODO
+    const double deltaGeom = 1e-15;
+    //fill charachteristic lengths for all spatial variables
+    GridWrapper<UnstructuredGrid> grid(this);
+    Vector characteristicLength = grid.calculateInfo().cellOverallDimension().first;
+    //check for degenerated cells with coincident points
+    internal::CsrHolder cn;
+    std::vector<bool> usefulCells(nc(), false);
+    std::vector<bool> usefulPoints(np(), false);
+//    for (size_t c = 0; c < nc(); ++c)
+//    {
+//        const size_t cnp = csr->cell2points.neigs(c);
+//        if (cnp < 2)
+//        {
+//            continue;    // points are not consider as correct elements
+//        }
+//        if (csr->r[c] != 0) //boundary
+//        {
+//            usefulCells[c] = true;
+//            for (tidx_t ip = eptr[c]; ip < eptr[c + 1]; ++ip)
+//            {
+//                usefulPoints[eind[ip]] = true;
+//            }
+//            continue;
+//        }
+//        Element<Unconsistent3D> realElem(csr->types[c]);
+//        if ((elemPoints != 0 && cnp != elemPoints) || (elemPoints == 0 && realElem.dimension() < realDimension))
+//        {
+//            continue;
+//        }
+//        bool useless = false;
+//        for (tidx_t ip1 = eptr[c]; ip1 < eptr[c + 1]; ++ip1)
+//        {
+//            for (tidx_t ip2 = ip1 + 1; ip2 < eptr[c + 1]; ++ip2)
+//            {
+//                dim_t d = 0;
+//                for (d = 0; d < dim; ++d)
+//                    if (std::fabs(csr->mp(eind[ip1], d) - csr->mp(eind[ip2], d)) / characteristicLength[d] > deltaGeom)
+//                    {
+//                        break;
+//                    }
+//                if (d == dim)
+//                {
+//                    useless = true;
+//                    break;
+//                }
+//            }
+//            if (useless)
+//            {
+//                break;
+//            }
+//        }
+//        if (!useless)
+//        {
+//            usefulCells[c] = true;
+//            for (tidx_t ip = eptr[c]; ip < eptr[c + 1]; ++ip)
+//            {
+//                usefulPoints[eind[ip]] = true;
+//            }
+//        }
+//    }
+//    //erase useless points and cell from vectors
+//    const dim_t newnc = static_cast<dim_t>(std::count(usefulCells.begin(), usefulCells.end(), true));
+//    const dim_t nctot = csr->nComponents();
+//    HostCsrVector<double> f(newnc, nctot, 0.0);
+//    tidx_t curID = 0;
+//    HostCsrVector<ElementType>& types = csr->types;
+//    for (dim_t c = 0; c < nc; ++c)
+//    {
+//        if (usefulCells[c])
+//        {
+//            std::copy(eind.getPointer(eptr[c]), eind.getPointer(eptr[c + 1]), eind.getPointer(eptr[curID]));
+//            eptr[curID + 1] = eptr[curID] + (eptr[c + 1] - eptr[c]);
+//            for (dim_t ifc = 0; ifc < nctot; ++ifc)
+//            {
+//                f(curID, ifc) = csr->f(c, ifc);
+//            }
+//            r[curID] = r[c];
+//            types[curID] = types[c];
+//            curID++;
+//        }
+//    }
+//    csr->f.swap(f);
+//    eptr.resize(newnc + 1, 1);
+//    r.resize(newnc, 1);
+//    types.resize(newnc, 1);
+//    eind.resize(eptr.back() / eind.ysize(), eind.ysize());
+
+//    curID = 0;
+//    std::vector<tidx_t> pointsMapping(np);
+//    const dim_t newnp = static_cast<dim_t>(std::count(usefulPoints.begin(), usefulPoints.end(), true));
+//    HostCsrVector<double> mp(newnp, dim, 0.0);
+//    for (dim_t p = 0; p < np; ++p)
+//    {
+//        if (usefulPoints[p])
+//        {
+//            for (dim_t d = 0; d < dim; ++d)
+//            {
+//                mp(curID, d) = csr->mp(p, d);
+//            }
+//            pointsMapping[p] = curID;
+//            curID++;
+//        }
+//    }
+//    csr->mp.swap(mp);
+
+//    for (size_t c = 0; c < newnc; ++c)
+//    {
+//        for (tidx_t ip = eptr[c]; ip < eptr[c + 1]; ++ip)
+//        {
+//            eind[ip] = pointsMapping[eind[ip]];
+//        }
+//    }
+
+//    return realDimension;
 }
 
 void UnstructuredGrid::reorderNeighbors()
